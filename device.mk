14df5f1ff0b6947405593048c4627993450aeece
Alessandro Astone <ales.astone@gmail.com>
2001-09-17 00:00:00 +0530

    begonia: Build and enable fastbootd mode
    Change-Id: I7f4246f3f583dcae7b3bfc0d3ae1cf4d577632cb
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>

--- device.mk
+++ device.mk
@@ -88,6 +88,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     FMRadio
 
+# Fastbootd
+PRODUCT_PACKAGES += \
+    fastbootd
+
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-service \
