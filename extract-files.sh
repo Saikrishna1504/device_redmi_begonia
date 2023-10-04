3d180960f8a8805ef1da22eaa62206978b73b6ba
Tim Zimmermann <tim@linux4.de>
2023-10-04 19:47:23 +0530

    begonia: Patch widevine to load libprotobuf-cpp-full-3.9.1
    Change-Id: I8d1fbc40bebf66abea342780b02cda68d64899b1
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -86,6 +86,12 @@ function blob_fixup {
         vendor/bin/hw/android.hardware.thermal@2.0-service.mtk)
             "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
             ;;
+        vendor/lib64/libwvhidl.so)
+            "${PATCHELF}" --replace-needed "libprotobuf-cpp-lite-3.9.1.so" "libprotobuf-cpp-full-3.9.1.so" "${2}"
+            ;;
+        vendor/lib64/mediadrm/libwvdrmengine.so)
+            "${PATCHELF}" --replace-needed "libprotobuf-cpp-lite-3.9.1.so" "libprotobuf-cpp-full-3.9.1.so" "${2}"
+            ;;
     esac
 }
 
