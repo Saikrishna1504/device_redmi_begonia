58aa1eea5a628d5526aa47e96fea262cbb38482b
Saikrishna1504 <saikrishna26918@gmail.com>
2023-10-31 22:07:25 +0530

    begonia: Drop:libprotobuf-cpp-lite-3.9.1-vendorcompat.
    *We don't need this .
    
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>

--- device.mk
+++ device.mk
@@ -253,7 +253,6 @@ PRODUCT_PACKAGES += \
 # Protobuf
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-full-3.9.1-vendorcompat \
-    libprotobuf-cpp-lite-3.9.1-vendorcompat
 
 # Public Libraries
 PRODUCT_COPY_FILES += \
