b19677c898b870d9c616a1fed9f4cc3ed5f86691
Saikrishna1504 <saikrishna26918@gmail.com>
2023-11-09 20:40:26 +0530

    Revert "begonia: rootdir: Introduce 'SafailNet'" * It's ded from long time,idk why i have this . * Even enforcing build showing permissive in settings page of this,it can't read selinux status properly
    This reverts commit e37a18af122edf567737c7f8cc2bc5c3300397d5.

--- device.mk
+++ device.mk
@@ -283,7 +283,6 @@ PRODUCT_PACKAGES += \
     init.modem.rc \
     init.mt6785.rc \
     init.mt6785.usb.rc \
-    init.safailnet.rc \
     init.project.rc \
     init.sensor_1_0.rc \
     fstab.mt6785 \
