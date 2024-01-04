d9c5e80e2b603046fc7d6ecdf367b6c983c3e553
Ryan _ <108048963+RyanYuuki@users.noreply.github.com>
2024-01-05 04:17:35 +0530

    Update Constants.java

--- parts/src/org/lineageos/settings/thermal/Constants.java
+++ parts/src/org/lineageos/settings/thermal/Constants.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.lineageos.settings.device.thermal;
+package org.lineageos.settings.thermal;
 
 public class Constants {
 
