7ab0a58125355ee9aa7a6f8d96d70011eee0d492
Saikrishna1504 <saikrishna26918@gmail.com>
2024-01-07 21:41:20 +0530

    begonia: Update Vendorsetup
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,12 +2,12 @@ rm -rf device/redmi
 git clone --depth=1 https://github.com/ProjectElixir-Devices/device_redmi_begonia -b UNO device/redmi/begonia
 
 rm -rf device/mediatek
-git clone --depth=1 https://github.com/Saikrishna1504/device_mediatek_sepolicy -b udc device/mediatek/sepolicy
+git clone --depth=1 https://github.com/Saikrishna1504/device_mediatek_sepolicy -b UNO device/mediatek/sepolicy
 
 rm -rf vendor/redmi
 git clone --depth=1 https://github.com/Saikrishna1504/vendor_redmi_begonia -b udc vendor/redmi/begonia
 git clone --depth=1 https://github.com/Saikrishna1504/vendor_redmi_begonia-ims -b udc vendor/redmi/begonia-ims
-git clone --depth=1 https://github.com/Saikrishna1504/vendor_redmi_begonia-firmware -b udc device/redmi/begonia-firmware
+git clone --depth=1 https://github.com/Saikrishna1504/vendor_redmi_begonia-firmware -b udc vendor/redmi/begonia-firmware
 
 rm -rf kernel/xiaomi
 git clone --depth=1 https://github.com/Saikrishna1504/kernel_xiaomi_mt6785 -b udc kernel/xiaomi/mt6785
@@ -20,3 +20,5 @@ git clone --depth=1 -b lineage-21.0 https://github.com/LineageOS/android_hardwar
 
 rm -rf packages/apps/MtkFMRadio
 git clone --depth=1 https://github.com/begonia-dev/android_packages_apps_MtkFMRadio packages/apps/MtkFMRadio
+
+
