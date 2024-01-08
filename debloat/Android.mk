b08dd9b9497a9d46187cc20583f316bfa47bf264
Saikrishna1504 <saikrishna26918@gmail.com>
2024-01-08 12:59:42 +0530

    begonia: Update Debloat Packages
    * Let's fix clumsiness
    
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>

--- debloat/Android.mk
+++ debloat/Android.mk
@@ -1,21 +1,54 @@
 LOCAL_PATH := $(call my-dir)
+
 include $(CLEAR_VARS)
 LOCAL_MODULE := Debloat
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES += AmbientSensePrebuilt AppDirectedSMSService arcore
-LOCAL_OVERRIDES_PACKAGES += CarrierSetup ConnMO ConnMetrics
-LOCAL_OVERRIDES_PACKAGES += DCMO  DiagnosticsToolPrebuilt DMService
-LOCAL_OVERRIDES_PACKAGES += GCS GoogleCamera GoogleTTS Jelly
-LOCAL_OVERRIDES_PACKAGES += MaestroPrebuilt Maps MyVerizonServices NovaBugReportWrapper Ornament OemDmTrigger OBDM_Perm obdm_stub
-LOCAL_OVERRIDES_PACKAGES += Phonograph PixelLiveWallpaperPrebuilt
-LOCAL_OVERRIDES_PACKAGES += RecorderPrebuilt
-LOCAL_OVERRIDES_PACKAGES += SafetyHubPrebuilt SCONE ScribePrebuilt Showcase SimpleCalculator SimpleCalendar Snap SoundAmplifierPrebuilt SprintDM SprintHM
-LOCAL_OVERRIDES_PACKAGES += Tag Tycho
-LOCAL_OVERRIDES_PACKAGES += USCCDM
-LOCAL_OVERRIDES_PACKAGES += Videos VZWAPNLib VzwOmaTrigger
-LOCAL_OVERRIDES_PACKAGES += WallpapersBReel2020
-LOCAL_OVERRIDES_PACKAGES += YouTubeMusicPrebuilt
+LOCAL_OVERRIDES_PACKAGES := \
+    AmbientSensePrebuilt \
+    AmbientStreaming \
+    arcore \
+    Camera2 \
+    CarrierLocation \
+    CarrierMetrics \
+    ConnMO \
+    ConnMetrics \
+    DCMO \
+    DevicePolicyPrebuilt \
+    DiagnosticsToolPrebuilt \
+    DMService \
+    Drive \
+    Gallery2 \
+    GCS \
+    GoogleCamera \
+    Maps \
+    Music \
+    MyVerizonServices \
+    OBDM_Permissions \
+    obdm_stub \
+    OdadPrebuilt \
+    OemDmTrigger \
+    OPScreenRecord \
+    Ornament \
+    PixelLiveWallpaperPrebuilt \
+    PlayAutoInstallConfig \
+    PrebuiltGmail \
+    RecorderPrebuilt \
+    SCONE \
+    ScribePrebuilt \
+    Showcase \
+    Snap \
+    Snap2 \
+    SnapdragonCamera \
+    SoundAmplifierPrebuilt \
+    SprintDM \
+    SprintHM \
+    USCCDM \
+    Videos \
+    VZWAPNLib \
+    VzwOmaTrigger \
+    YouTube \
+    YouTubeMusicPrebuilt
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
