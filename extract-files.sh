85f95f1f26b93f0a97aad36b8a6edb1508a21236
Saikrishna1504 <saikrishna26918@gmail.com>
2024-01-31 15:07:37 +0000

    begonia: Make shim for libem_support_jni.so * dlopen failed: cannot locate symbol "_ZN7android21SurfaceComposerClient23getInternalDisplayTokenEv" referenced by "/system/lib64/libem_support_jni.so"...
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -102,6 +102,9 @@ function blob_fixup {
         vendor/bin/hw/android.hardware.keymaster@4.0-service.beanpod)
             "${PATCHELF}" --add-needed "libshim_beanpod.so" "${2}"
             ;;
+        lib64/libem_support_jni.so)
+            "${PATCHELF}" --add-needed "libjni_shim.so" "${2}"
+            ;;
     esac
 }
 
