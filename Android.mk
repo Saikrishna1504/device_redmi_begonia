1ad6ac20c9601a3db14b53cbc33add096e3a0f47
SamarV-121 <samar@samarv121.dev>
2024-11-12 12:00:00 +0530

    begonia: Auto generate symlinks while extraction  - depends on https://review.lineageos.org/c/LineageOS/android_tools_extract-utils/+/386540
    Change-Id: I6846c11c7ed2fa2eb7d237321f7d30677c311d05
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>

--- Android.mk
+++ Android.mk
@@ -4,31 +4,4 @@ ifeq ($(TARGET_DEVICE),begonia)
 
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 
-include $(CLEAR_VARS)
-
-ENGMODE_LIBS := libem_support_jni.so libjni_shim.so
-ENGMODE_SYMLINKS := $(addprefix $(TARGET_OUT_APPS_PRIVILEGED)/EngineerMode/lib/arm64/,$(notdir $(ENGMODE_LIBS)))
-$(ENGMODE_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@echo "EngineerMode libs symlinks: $@"
-	@mkdir -p $(dir $@)
-	@rm -rf $@
-	$(hide) ln -sf /system/lib64/$(notdir $@) $@
-
-VENDOR_SYMLINKS := \
-    $(TARGET_OUT_VENDOR)/lib/hw \
-    $(TARGET_OUT_VENDOR)/lib64/hw
-
-$(VENDOR_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	$(hide) echo "Making vendor symlinks"
-	@mkdir -p $(TARGET_OUT_VENDOR)/lib/hw
-	@mkdir -p $(TARGET_OUT_VENDOR)/lib64/hw
-	@ln -sf libSoftGatekeeper.so $(TARGET_OUT_VENDOR)/lib/hw/gatekeeper.default.so
-	@ln -sf libSoftGatekeeper.so $(TARGET_OUT_VENDOR)/lib64/hw/gatekeeper.default.so
-	@ln -sf /vendor/lib/egl/libGLES_mali.so $(TARGET_OUT_VENDOR)/lib/hw/vulkan.mt6785.so
-	@ln -sf /vendor/lib64/egl/libGLES_mali.so $(TARGET_OUT_VENDOR)/lib64/hw/vulkan.mt6785.so
-	$(hide) touch $@
-
-ALL_DEFAULT_INSTALLED_MODULES += $(ENGMODE_SYMLINKS)
-ALL_DEFAULT_INSTALLED_MODULES += $(VENDOR_SYMLINKS)
-
 endif
