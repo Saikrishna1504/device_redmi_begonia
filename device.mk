bccc52bef7df4de14b7155ef6fb5dc0a1321174a
Michael Bestas <mkbestas@lineageos.org>
2025-05-17 08:29:21 +0530

    begonia: Move Lineage Health HAL to select() - Its deprecated
    vendor/lineage/config/BoardConfigSoong.mk:66: warning: TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_BYPASS is deprecated, please migrate to soong_config_set,lineage_health,charging_control_supports_bypass
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>

--- device.mk
+++ device.mk
@@ -46,6 +46,8 @@ PRODUCT_PACKAGES += \
     BesLoudness
 
 # Lineage Health
+$(call soong_config_set,lineage_health,charging_control_supports_bypass,false)
+
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
