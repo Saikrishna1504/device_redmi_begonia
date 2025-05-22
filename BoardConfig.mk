a48ff6b1ad0ec4c06c84759b85c409d6f62e424b
Saikrishna1504 <saikrishna26918@gmail.com>
2025-05-22 15:32:59 +0000

    [TMP] begonia: Set SELinux to permissive
    Change-Id: I5b4cc379f5bdde2e81ae332d4a1ffb3b0d250ddb
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -43,6 +43,7 @@ TARGET_SCREEN_DENSITY := 440
 TARGET_BOARD_PLATFORM := mt6785
 
 # Kernel
+BOARD_BOOTCONFIG := androidboot.selinux=permissive
 BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 androidboot.init_fatal_reboot_target=recovery
 BOARD_KERNEL_BASE := 0x40078000
 BOARD_KERNEL_OFFSET := 0x00008000
