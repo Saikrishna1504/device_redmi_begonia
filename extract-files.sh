c54bdf6ec201c1f087dd9e79ccdfb1504ed6b4b6
Mashopy <eliasgheeraert@gmail.com>
2025-06-18 20:05:57 +0530

    begonia: Patch mtkfusionrild to load libutils-v32 * Fixes RIL in Android 16
    Change-Id: Ic9d3ecad755f5a7fcda7deccfbfbb31a305752c1
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -124,6 +124,10 @@ function blob_fixup {
              [ "$2" = "" ] && return 0
              grep -q "libbase_shim.so" "${2}" || "${PATCHELF}" --add-needed "libbase_shim.so" "${2}"
              ;;
+        vendor/bin/hw/mtkfusionrild)
+             [ "$2" = "" ] && return 0
+             "${PATCHELF}" --add-needed "libutils-v32.so" "${2}"
+             ;;
         *)
             return 1
             ;;
