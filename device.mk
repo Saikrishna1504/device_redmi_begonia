539f3ec0e24e16c7cabaf5754b7024735eee1b3d
Saikrishna1504 <saikrishna26918@gmail.com>
2025-08-18 15:17:54 +0000

    begonia: Enable Performance mode governor support
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>

--- device.mk
+++ device.mk
@@ -64,6 +64,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
+# Axion Performance Mode
+PERF_GOV_SUPPORTED := true
+PERF_DEFAULT_GOV := schedutil
+
 # BesLoudness
 PRODUCT_PACKAGES += \
     BesLoudness
