c588accf8ebb5ea69d12089fc7473691cb633b1b
WuXing90 <2096477910@qq.com>
2025-12-04 17:25:59 +0800

    begonia: Retrofit dynamic partitions
    Signed-off-by: wuxinG90 <2096477910df@gmail.com>
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>

--- device.mk
+++ device.mk
@@ -153,6 +153,10 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
+# Partitions
+PRODUCT_RETROFIT_DYNAMIC_PARTITIONS := true
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
