a6dfac90b9db948e4df5cd4a98d952d16e69dabd
claxten10 <claxten10@gmail.com>
2025-12-20 01:08:43 +0000

    begonia: Build gatekeeper hal as 64-bit only
    Change-Id: I272373343f894590aad28e8409191bc81ab75baa
    Signed-off-by: claxten10 <claxten10@gmail.com>

--- device.mk
+++ device.mk
@@ -99,7 +99,7 @@ PRODUCT_PACKAGES += \
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-service \
-    android.hardware.gatekeeper@1.0-impl
+    android.hardware.gatekeeper@1.0-impl:64
 
 # Health
 PRODUCT_PACKAGES += \
