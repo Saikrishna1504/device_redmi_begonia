e6a0ffe4c3f61df3c869611c835b528f22be277e
LuK1337 <priv.luk@gmail.com>
2025-12-25 15:15:42 +0000

    begonia: Opt out of speaker_layout_channel_mask field
    Required for ABI compatibility with audio HAL compiled before 15 QPR2.
    
    Fixes: https://gitlab.com/LineageOS/issues/android/-/issues/9426
    Change-Id: I0bf7eafbeb19957326de04ecf7b903580315dc59
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>

--- device.mk
+++ device.mk
@@ -16,6 +16,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
 # Audio
+$(call soong_config_set_bool,android_hardware_audio,skip_speaker_layout_channel_mask_field,true)
+
 PRODUCT_PACKAGES += \
     android.hardware.audio.service \
     android.hardware.audio@6.0-impl \
