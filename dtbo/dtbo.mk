81ec8a31714a39d6a4b0853f7a4fba619bf7be57
Saikrishna1504 <saikrishna26918@gmail.com>
2026-09-21 03:05:12 +0000

    shitty hack?
    Change-Id: I190d4e1a328b2cb30742cc93ae751e4f41f9b7eb

--- dtbo/dtbo.mk
+++ dtbo/dtbo.mk
@@ -8,5 +8,13 @@ $(BOARD_PREBUILT_DTBOIMAGE):
 	@echo "Building dtbo.img"
 	$(call make-dtbo-target,$(KERNEL_DEFCONFIG))
 	$(call make-dtbo-target,dtbs)
+	@if [ ! -s $(BOARD_DTBO_CFG) ]; then \
+		echo "dtboimg.cfg is empty! Generating a dummy DTBO overlay to satisfy BOARD_KERNEL_SEPARATED_DTBO..." ; \
+		mkdir -p $(DTBO_OUT)/arch/$(KERNEL_ARCH)/boot/dts/mediatek ; \
+		echo '/dts-v1/; /plugin/; / { fragment@0 { target-path = "/"; __overlay__ { dummy = <1>; }; }; };' > $(DTBO_OUT)/dummy.dts ; \
+		$(DTC) -@ -I dts -O dtb -o $(DTBO_OUT)/arch/$(KERNEL_ARCH)/boot/dts/mediatek/dummy.dtbo $(DTBO_OUT)/dummy.dts ; \
+		echo "mediatek/dummy.dtbo" > $(BOARD_DTBO_CFG) ; \
+		echo " id=0" >> $(BOARD_DTBO_CFG) ; \
+	fi
 	$(MKDTBOIMG) cfg_create $@ $(BOARD_DTBO_CFG) -d $(DTBO_OUT)/arch/$(KERNEL_ARCH)/boot/dts
 	$(APPEND_CERTS) --alignment 16 --cert1 $(DEVICE_PATH)/dtbo/cert1.der --cert2 $(DEVICE_PATH)/dtbo/cert2.der --dtbo $(BOARD_PREBUILT_DTBOIMAGE)
